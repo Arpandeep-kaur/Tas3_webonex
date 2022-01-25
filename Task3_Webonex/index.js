1494aa0d88bc13052020ff637d7571057043c15a
Arpandeep-kaur <82841771+Arpandeep-kaur@users.noreply.github.com>
2022-01-25 16:16:44 +0530

    Add files via upload

---
+++ Task3_Webonex/index.js
@@ -0,0 +1,14 @@
+/*
+ In the variables declare above assign the variables as appropriate default value instead of null.
+
+
+*/
+
+let name='xyz';
+let email='xyz@gmail.com';
+let age='22';
+let address='123, sector B';
+let phone_number=1234567821;
+let gender='Female';
+let isMarried=false;
+console.log(name,email,age,address,phone_number,gender,isMarried)
